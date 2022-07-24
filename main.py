51b07aecb2cad1546e58b33d5577a2b713446562
astrid-schindler <astrid.schindler91@icloud.com>
2022-07-24 13:35:32 +0200

    Added new function for side by side pairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # LISTS
-directions_list1 = ["NORDEN", "OSTEN", "WESTEN", "SUEDEN", "WESTEN", "WESTEN", "NORDEN"]
+directions_list1 = ["NORDEN", "OSTEN",  "WESTEN", "SUEDEN", "WESTEN", "WESTEN", "NORDEN"]
 directions_list2 = ["SUEDEN", "NORDEN", "SUEDEN", "WESTEN", "WESTEN", "OSTEN", "NORDEN", "NORDEN", "OSTEN", "OSTEN"]
 
 
@@ -25,5 +25,29 @@ def solve(directions):
     return directions
 
 
+# REMOVES SIDE BY SIDE PAIRS
+def solve_third(directions_third):
+
+    for idx_a, a in enumerate(directions_third):
+        if a == "NORDEN" and ((idx_a + 1) < len(directions_third)) and directions_third[idx_a + 1] == "SUEDEN":
+            directions_third.pop(idx_a)
+            directions_third.pop(idx_a)
+            solve_third(directions_third)
+        elif a == "SUEDEN" and ((idx_a + 1) < len(directions_third)) and directions_third[idx_a + 1] == "NORDEN":
+            directions_third.pop(idx_a)
+            directions_third.pop(idx_a)
+            solve_third(directions_third)
+        elif a == "OSTEN" and ((idx_a + 1) < len(directions_third)) and directions_third[idx_a + 1] == "WESTEN":
+            directions_third.pop(idx_a)
+            directions_third.pop(idx_a)
+            solve_third(directions_third)
+        elif a == "WESTEN" and ((idx_a + 1) < len(directions_third)) and directions_third[idx_a + 1] == "OSTEN":
+            directions_third.pop(idx_a)
+            directions_third.pop(idx_a)
+            solve_third(directions_third)
+
+    return directions_third
+
+
 # TESTING FUNCTION
-print(solve(directions_list1))
+print(solve(directions_list2))
